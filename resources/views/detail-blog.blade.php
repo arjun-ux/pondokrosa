19ab58b3cecfaba50b515efffa2c823641b4b272
arjun-ux <arux899@gmail.com>
2025-03-30 01:00:42 +0700

    fix: image detail-blog

--- resources/views/detail-blog.blade.php
+++ resources/views/detail-blog.blade.php
@@ -42,7 +42,7 @@
 
                 <!-- Featured Image -->
                 <div class="article-featured-image fade-in">
-                    <img src="https://images.unsplash.com/photo-1580582932707-520aed937b7b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80" alt="Pendidikan Islam Terpadu">
+                    <img src="{{ $data['image'] ?: 'https://images.unsplash.com/photo-1580582932707-520aed937b7b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80' }}" alt="Pendidikan Islam Terpadu">
                 </div>
 
                 <!-- Article Content -->
